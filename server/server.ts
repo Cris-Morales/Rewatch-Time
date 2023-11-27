86b8f6ff9307e2eaa911d8738b1b34cde7db7380
Cristian Morales <crismorales@protonmail.com>
2023-11-27 14:04:05 -0600

    setup server boilerplate. Typescript, Node, Express, with Hot Reloading using the compiled js file.

---
+++ server/server.ts
@@ -0,0 +1,47 @@
+import express, { NextFunction, Request, Response } from 'express';
+import fs from 'fs';
+import process from 'process';
+
+interface ServerError {
+  log: string;
+  status: number;
+  message: {
+    err: string;
+  };
+}
+
+const app = express();
+const port = 44000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+
+// Handle request to unknown endpoints
+app.use('/', (req: Request, res: Response): Response => {
+  return res.status(404).json({ error: 'Endpoint does no exist...' });
+});
+
+//Global Error Handler
+app.use(
+  (
+    err: ServerError,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Response => {
+    const defaultErr: ServerError = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An Error Occurred' },
+    };
+    const errorObj: ServerError = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  },
+);
+
+app.listen(44000, (): void => {
+  console.log(`Listening on port: 44000`);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:44000',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    },
+  },
 });
