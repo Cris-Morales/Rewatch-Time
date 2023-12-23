3b687700b7b24d5cee0740cde0de758f781aba62
Cristian Morales <crismorales@protonmail.com>
2023-12-23 12:11:37 -0600

    beginning more form features.

--- src/Generator/Generator.tsx
+++ src/Generator/Generator.tsx
@@ -29,55 +29,81 @@ const Generator = (): JSX.Element => {
     setShowPlatlist(true);
   };
 
+  // onChange={e => {
+  //   setFinale(e.target.checked);
+  //   if (e.target.checked) {
+  //     setPlaylistTime(playlistTime + 44);
+  //   } else {
+  //     setPlaylistTime(playlistTime - 44);
+  //   }
+  // }}
+  // onChange={e => {
+  //   let newLength: number = e.target.valueAsNumber;
+  //   setPlaylistLength(newLength);
+  //   setPlaylistTime(newLength * 11 + (finale ? 44 : 0));
+  // }}
+  // Watchtime: {playlistTime} Minutes
+
   return (
-    <div className='bottom-0 left-0 w-screen  bg-blue-950 text-white flex flex-col justify-evenly items-center h-section'>
+    <div className=' bottom-0 left-0 w-screen  bg-blue-950 text-black flex flex-col justify-evenly items-center h-section border-2 border-solid border-red-600 font-thunderman'>
       <div className='flex justify-center items-center h-fit'>
         <img src={marceline} width='600px' className='mr-10' />
-        <div>
-          <form id='generator' className='h-96 w-96 bg-gray-500'>
-            <label>Playlist Length</label>
-            <input
-              className='text-black'
-              id='playlistLength'
-              type='number'
-              form='generator'
-              max='280'
-              min='0'
-              onChange={e => {
-                let newLength: number = e.target.valueAsNumber;
-                setPlaylistLength(newLength);
-                setPlaylistTime(newLength * 11 + (finale ? 44 : 0));
-              }}></input>
-            <label>Include Finale</label>
-            <input
-              type='checkbox'
-              id='finale'
-              onChange={e => {
-                setFinale(e.target.checked);
-                if (e.target.checked) {
-                  setPlaylistTime(playlistTime + 44);
-                } else {
-                  setPlaylistTime(playlistTime - 44);
-                }
-              }}></input>
-            Watchtime: {playlistTime} Minutes
+        <div className='flex flex-col items-center'>
+          <form
+            id='generator'
+            className='h-96 w-96 bg-white p-5 flex flex-col rounded-3xl'>
+            <div className='flex justify-between items-center'>
+              <label>Max Playlist Length</label>
+              <input
+                className='input input-bordered text-white text-center w-1/4 rounded text-align pt-1.5 pr-3'
+                id='playlistLength'
+                type='number'
+                form='generator'
+                max='280'
+                min='0'
+              />
+              <p>episodes</p>
+            </div>
+            <div className='flex items-center '>
+              <label>Include Finale</label>
+              <input
+                type='checkbox'
+                id='finale'
+                className='my-2 ml-4  checkbox bg bg-gray-800'
+                onChange={e => {
+                  setFinale(e.target.checked);
+                }}></input>
+            </div>
+            <div>
+              <label>Include/Exclude Arcs</label>
+            </div>
+            <div>
+              <label>Include Watched Episodes</label>
+            </div>
+            <div>
+              <label>Random or from watched episode</label>
+            </div>
+            <div>
+              <label>Include/Exclude Seasons</label>
+            </div>
           </form>
           <button
-            className='navbar-button'
+            className='navbar-button self-center text-white'
             type='submit'
             form='generator'
             value='Submit'
             onClick={handleSubmit}>
             Generate Playlist
+            {/* change to get new playlist after the initial playlist is gotten */}
           </button>
         </div>
         <img src={FinnJakeRelax} width='600px' className='ml-10' />
       </div>
-      {showPlaylist ? (
-        <Carousel playlist={playlist} />
-      ) : (
-        <div className='flex h-card w-screen justify-center items-center z-10'></div>
-      )}
+      {
+        showPlaylist ? <Carousel playlist={playlist} /> : null
+        // <div className='flex h-card w-screen justify-center items-center z-10'></div>
+      }
+      {/* ^ I want it to slide or fade into the div. */}
     </div>
   );
 };
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -15,7 +15,9 @@ const Home = (): JSX.Element => {
       }}>
       {/* <div className='flex justify-center items-center bg-gray-200 bg-opacity-95 w-3/5'> */}
       <div className='flex flex-col justify-center items-center backdrop-blur-xl w-3/5 h-subsection'>
-        <div className='font-thunderman text-7xl mt-20'>Welcome!</div>
+        <div className='font-thunderman text-black text-7xl mt-20'>
+          Welcome!
+        </div>
         <div className='flex justify-center items-center'>
           <img
             className='mt-10 mb-10 mx-auto'
@@ -23,7 +25,7 @@ const Home = (): JSX.Element => {
             height='auto'
             src={finnAndJake}
           />
-          <div className='mt-5 mb-10 mr-10 ml-10 bg-white px-5 py-5 w-96 rounded-3xl font-thunderman'>
+          <div className='text-black mt-5 mb-10 mr-10 ml-10 bg-white px-5 py-5 w-96 rounded-3xl font-thunderman'>
             Adventure Time is a show about a boy named Finn, his best friend
             Jake and their adventures in the land of Ooo. It aired on Cartoon
             Network from 2010 to 2018, with an additional miniseries, "Distant
