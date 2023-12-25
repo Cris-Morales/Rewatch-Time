30a33d550719ef0bf84568f67a2657d2365328b4
Cristian Morales <crismorales@protonmail.com>
2023-12-25 15:59:42 -0600

    fixed playlist length, while keeping the modularized components.

--- src/Generator/FormContainer.tsx
+++ src/Generator/FormContainer.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from 'react';
-
+import SeriesForm from './FormModules/SeriesForm.tsx';
+import ArcsForm from './FormModules/ArcsForm.tsx';
+import SelectWatchedForm from './FormModules/SelectWatchedForm.tsx';
+import SpecifyEpisodes from './FormModules/SpecifyEpisodes.tsx';
 
 const FormContainer = ({
   seriesList,
@@ -13,7 +16,51 @@ const FormContainer = ({
   // behind the scenes, if not loged in, we can't let the user make this query.
 
   return (
-
+    <div
+      id='generator'
+      className='text-black h-fit w-generatorForm bg-white p-5 flex flex-col rounded-3xl items-center shadow-lg'>
+      <div className='my-1 mx- w-full flex items-center justify-between'>
+        <h3 className=' text-2xl'>Playlist Generator</h3>
+        <p>Is Logged In: {loggedIn ? 'true' : 'false'}</p>
+      </div>
+      <div className='form-control'>
+        <label className='label cursor-pointer flex justify-center'>
+          <span className='mr-2 text-center'>Randomized Playlist</span>
+          <input
+            id='queryType'
+            type='checkbox'
+            className='toggle'
+            disabled={!loggedIn}
+          />
+          <span className='text-center ml-2'>
+            Chronologically from Watched Episode
+          </span>
+        </label>
+      </div>
+      <div className='flex justify-center items-center'>
+        <div className='flex items-center justify-center w-full mx-5'>
+          <label className='mr-2'>Max Playlist Length</label>
+          <input
+            className='mr-2 input input-bordered text-center w-1/4 rounded text-align pt-1.5 pr-3'
+            id='playlistLength'
+            type='number'
+            form='generator'
+            max='280'
+            min='1'
+            defaultValue={1}
+            onChange={e => {
+              let newLength: number = e.target.valueAsNumber;
+              setPlaylistLength(newLength);
+            }}
+          />
+          <p className=''>episodes</p>
+        </div>
+        <SpecifyEpisodes />
+      </div>
+      <SeriesForm seriesList={seriesList} seasonSeries={seasonSeries} />
+      <ArcsForm arcs={arcs} />
+      <SelectWatchedForm />
+    </div>
   );
 };
 
--- src/Generator/Generator.tsx
+++ src/Generator/Generator.tsx
@@ -4,10 +4,6 @@ import query, { useQuery, useQueryClient } from '@tanstack/react-query';
 import fetchPlaylist from '../fetchPlaylist';
 import fetchArcList from '../fetchArcList';
 import FormContainer from './FormContainer';
-import SeriesForm from './FormModules/SeriesForm.tsx';
-import ArcsForm from './FormModules/ArcsForm.tsx';
-import SelectWatchedForm from './FormModules/SelectWatchedForm.tsx';
-import SpecifyEpisodes from './FormModules/SpecifyEpisodes.tsx';
 const marceline: string = 'assets/Marceline.png';
 const FinnJakeRelax: string = 'assets/FinnJakeRelax.png';
 const bmo: string = 'arcs/BMO.webp';
@@ -62,7 +58,7 @@ const Generator = (): JSX.Element => {
   const [finale, setFinale] = useState<boolean>(false);
   const [showPlaylist, setShowPlatlist] = useState<boolean>(false);
   const [excludedArcs, setExcludedArcs] = useState<string[]>([]);
-  const loggedIn: boolean = true;
+  const loggedIn: boolean = false;
   const [queryType, setQueryType] = useState<boolean>(false); // False is randomly generated, true is chronological
   const [playlistLength, setPlaylistLength] = useState<number>(1);
 
@@ -95,50 +91,14 @@ const Generator = (): JSX.Element => {
       <div className='flex justify-center items-center h-fit'>
         <img src={marceline} width='600px' className='mr-10' />
         <div className='flex flex-col items-center'>
-          <div
-            id='generator'
-            className='text-black h-fit w-generatorForm bg-white p-5 flex flex-col rounded-3xl items-center shadow-lg'>
-            <div className='my-1 mx- w-full flex items-center justify-between'>
-              <h3 className=' text-2xl'>Playlist Generator</h3>
-              <p>Is Logged In: {loggedIn ? 'true' : 'false'}</p>
-            </div>
-            <div className='form-control'>
-              <label className='label cursor-pointer flex justify-center'>
-                <span className='mr-2 text-center'>Randomized Playlist</span>
-                <input
-                  id='queryType'
-                  type='checkbox'
-                  className='toggle'
-                  disabled={!loggedIn}
-                />
-                <span className='text-center ml-2'>
-                  Chronologically from Watched Episode
-                </span>
-              </label>
-            </div>
-            <div className='flex justify-center items-center'>
-              <div className='flex items-center justify-center w-full mx-5'>
-                <label className='mr-2'>Max Playlist Length</label>
-                <input
-                  className='mr-2 input input-bordered text-center w-1/4 rounded text-align pt-1.5 pr-3'
-                  id='playlistLength'
-                  type='number'
-                  form='generator'
-                  max='280'
-                  min='1'
-                  defaultValue={1}
-                  onChange={e => {
-                    setPlaylistLength(e.);
-                  }}
-                />
-                <p className=''>episodes</p>
-              </div>
-              <SpecifyEpisodes />
-            </div>
-            <SeriesForm seriesList={seriesList} seasonSeries={seasonSeries} />
-            <ArcsForm arcs={arcs} />
-            <SelectWatchedForm />
-          </div>
+          <FormContainer
+            seriesList={seriesList}
+            arcs={arcs}
+            seasonSeries={seasonSeries}
+            loggedIn={loggedIn}
+            queryType={queryType}
+            setPlaylistLength={setPlaylistLength}
+          />
           <button
             className='navbar-button self-center text-white text-lg'
             type='submit'
@@ -152,8 +112,7 @@ const Generator = (): JSX.Element => {
         <img src={FinnJakeRelax} width='600px' className='ml-10' />
       </div>
       {
-        // showPlaylist ? <Carousel playlist={playlist} /> : null
-        showPlaylist ? playlistLength : null
+        showPlaylist ? <Carousel playlist={playlist} /> : null
 
         // <div className='flex h-card w-screen justify-center items-center z-10'></div>
       }
