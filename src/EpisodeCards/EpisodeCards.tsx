c6baa4041275f7ccea3c3782b51aaad0b0ccf477
Cristian Morales <crismorales@protonmail.com>
2023-12-28 21:21:04 -0600

    fixed error log in client side in episode card.

--- src/EpisodeCards/EpisodeCards.tsx
+++ src/EpisodeCards/EpisodeCards.tsx
@@ -75,13 +75,13 @@ const EpisodeCard = ({ episode }): JSX.Element => {
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
               viewBox='0 0 24 24'
-              stroke-width='3.0'
+              strokeWidth='3.0'
               stroke='currentColor'
               data-slot='icon'
               className='w-6 h-6'>
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
+                strokeLinecap='round'
+                strokeLinejoin='round'
                 d='M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3'
               />
             </svg>
